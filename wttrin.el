6cd918dcd279692bb9060b7dad3d0ae89612e4a8
chen-chao <wenbushi@gmail.com>
2019-02-27 20:04:06 +0800

    show weather in mode line

--- wttrin.el
+++ wttrin.el
@@ -16,6 +16,7 @@
 
 (require 'url)
 (require 'xterm-color)
+(require 'timer)
 
 (defgroup wttrin nil
   "Emacs frontend for weather web service wttr.in."
@@ -33,6 +34,33 @@
   :type '(list)
   )
 
+(defcustom wttrin-mode-line-city nil
+  "Specify default city for displaying weather in mode line.
+nil for automatically chosen by wttr.in."
+  :group 'wttrin
+  :type 'string
+  )
+
+(defcustom wttrin-mode-line-format "format=\"%l:+%c %t %w\""
+  "Specify default information format for querying wttr.in"
+  :group 'wttrin
+  :type 'string
+  )
+
+(defcustom wttrin-mode-line-time-interval 3600
+  "Speify the fetching interval of querying weather information."
+  :group 'wttrin
+  :type 'number
+  )
+
+(defvar wttrin-mode-line-timer nil)
+
+(defvar wttrin-weather-string nil
+  "String used in mode lines to display the weather
+It should not be set directly, but is instead updated by the
+`wttrin-display-weather-in-mode-line' function.")
+;;;###autoload(put 'wttrin-weather-string 'risky-local-variable t)
+
 (defun wttrin-fetch-raw-string (query)
   "Get the weather information based on your QUERY."
   (let ((url-request-extra-headers '(("User-Agent" . "curl"))))
@@ -42,6 +70,54 @@
          (concat "http://wttr.in/" query)
          (lambda (status) (switch-to-buffer (current-buffer))))
       (decode-coding-string (buffer-string) 'utf-8))))
+
+(defun wttrin-fetch-info (&optional city-name format)
+  "Fetch the weather information by city name and query format."
+  (let* ((city (or city-name ""))
+	 (query-string (if format
+			   (concat city "?" format)
+			 city))
+	 (raw-string (wttrin-fetch-raw-string query-string))
+	 (pos (+ 2 (string-match "^$" raw-string)))  ; get rid of the quotation marks
+	 (msg (substring raw-string pos -2))
+	 )
+    msg)
+)
+
+(defun wttrin-info-update ()
+  (let ((city (or wttrin-mode-line-city ""))
+	(format wttrin-mode-line-format))
+    (condition-case ()
+	(setq weather-info (wttrin-fetch-info city format))
+      (error "")
+      )
+    (setq wttrin-weather-string weather-info)
+    )
+  (force-mode-line-update)
+  )
+
+(defun wttrin-display-weather-in-mode-line ()
+  (and wttrin-mode-line-timer (cancel-timer wttrin-mode-line-timer))
+  (setq wttrin-mode-line-timer nil)
+  (setq wttrin-weather-string "")
+  (or global-mode-string (setq global-mode-string '("")))
+  (or (memq 'wttrin-weather-string global-mode-string)
+      (setq global-mode-string
+	    (append global-mode-string '(wttrin-weather-string))))
+  (setq wttrin-mode-line-timer
+	(run-with-timer 0 wttrin-mode-line-time-interval
+			(lambda () (make-thread 'wttrin-info-update))))
+  ;; (wttrin-info-update)
+  )
+
+(defun wttrin-cancel-display-in-mode-line ()
+  (interactive)
+  (and wttrin-mode-line-timer (cancel-timer wttrin-mode-line-timer))
+  (setq wttrin-mode-line-timer nil)
+  (setq wttrin-weather-string "")
+  (delete 'wttrin-weather-string global-mode-string)
+  (force-mode-line-update)
+  )
 
 (defun wttrin-exit ()
   (interactive)
